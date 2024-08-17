a1e313a2f091953fcc449901f53233740404ac3f
SummitCraft <lennoxjude923@outlook.com>
2024-08-17 14:25:37 +0300

    Update BurpExtender.java

--- SAMLReQuest/code/BurpExtender.java
+++ SAMLReQuest/code/BurpExtender.java
@@ -1,17 +1,11 @@
-/**
- * @author Ahmad Abolhadid <bo7adeed@gmail.com>
- */
-
 package burp;
 
-public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory
-{
+public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
      
     @Override
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
-    {
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         this.callbacks = callbacks;
         this.helpers = callbacks.getHelpers();
         callbacks.setExtensionName("SAMLReQuest 2.0");
@@ -20,12 +14,8 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory
         callbacks.registerHttpListener(proc);
     }
 
-
     @Override
-    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable)
-    {
-        return new SamlRequestTab(controller, editable,callbacks);
+    public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable) {
+        return new SamlRequestTab(controller, editable, callbacks);
     }
-
-
 }
